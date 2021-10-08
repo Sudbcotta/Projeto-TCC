d26815d3275de15a0503489c9c7bc60474e15f37
Sudbcotta <32546400+Sudbcotta@users.noreply.github.com>
2021-10-08 09:35:15 -0300

    Add files via upload

---
+++ notwork1/Javascript/main.js
@@ -0,0 +1,15 @@
+$('input').on('change', function() {
+    $('body').toggleClass('blue');
+  });
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  } 
+
+
+  
+ 
